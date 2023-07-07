c152e33b75b04004f3dc839af737e360857360b2
Macko <prvinsm21@gmail.com>
2023-07-07 14:58:41 +0530

    Files added

---
+++ src/test/java/com/macko/cofeeshop/CofeeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macko.cofeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofeeshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
